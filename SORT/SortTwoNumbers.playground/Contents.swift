b67b865a0c8c3ea2519d2e96311392a30805300a
imkh2 <121610164+imkh2@users.noreply.github.com>
2023-01-11 23:09:16 +0300

    Add files via upload

---
+++ SORT/SortTwoNumbers.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func sortNumbers (a: Int, b: Int) -> (Int, Int) {
+    if a<b {
+        return (a,b)
+    }
+    else {
+        return (b,a)
+    }
+}
+print(sortNumbers(a: 8, b: 7))
+
+
+
+
+//Sorting Three Numbers
+
+
+
